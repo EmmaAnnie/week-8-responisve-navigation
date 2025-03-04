d7501f3450010de5a13af6710cc266374bc1662b
EmmaAnnie <emmabuchta06@gmail.com>
2025-03-04 13:35:01 -0700

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -2,5 +2,5 @@ const navToggle = document.getElementById('nav-toggle');
 const menuList = document.getElementById('main-menu');
 
 navToggle.addEventListener('click', function(){
-    
+    menuList.classList.toggle('active')
 });
